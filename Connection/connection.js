aa7a5c948c19b8a5c1c862f1f3674f7d89c618c9
Sanjay <sanjaym1998@gmail.com>
2023-03-25 20:38:43 +0530

    url patch error

--- Connection/connection.js
+++ Connection/connection.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const URL="mongodb+srv://SanjayM:Mundasad@1998@cluster0.jfdlxyl.mongodb.net/instaclone?retryWrites=true&w=majority"
+const URL = "mongodb+srv://sanjaym1998:EhMHug730tnWkKCB@cluster0.jao4mrc.mongodb.net/Notes?retryWrites=true&w=majority";
 async function main(){
     await mongoose.connect(URL,{useNewUrlParser: true, useUnifiedTopology:true})
     console.log("connection done!");
