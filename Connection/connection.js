fce459e5a30a302695ed42c091548e2c7f84546a
Sanjay <sanjaym1998@gmail.com>
2023-03-25 17:12:44 +0530

    updated

--- Connection/connection.js
+++ Connection/connection.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const URL="mongodb+srv://SanjayM:Mundasad@1998@cluster0.jfdlxyl.mongodb.net/?retryWrites=true&w=majority"
+const URL="mongodb+srv://SanjayM:Mundasad@1998@cluster0.jfdlxyl.mongodb.net/instaclone?retryWrites=true&w=majority"
 async function main(){
     await mongoose.connect(URL,{useNewUrlParser: true, useUnifiedTopology:true})
     console.log("connection done!");
